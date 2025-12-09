c81b3187d29cd59ec663addd13002d7239865ecb
Gabriel Bastos de Freitas <gabrielbastosdev@gmail.com>
2025-12-08 21:24:05 -0300

    Daily LeetCode - #1925 Count Square Sum Triples

--- LeetCode/count_triples.py
+++ LeetCode/count_triples.py
@@ -1,4 +1,4 @@
-def countTriples(self, n: int) -> int:
+def countTriples(n: int) -> int:
         count = 0
         for c in range(1,n+1):
             for a in range(1, c):
@@ -9,3 +9,9 @@ def countTriples(self, n: int) -> int:
                 if b.is_integer():
                     count+=1
         return count
+
+if __name__ == "__main__":
+    n = 5
+    print(countTriples(n))  
+    m = 10
+    print(countTriples(m))
